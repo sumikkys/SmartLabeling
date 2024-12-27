7859e23590bc858d07af8784a9fa7f864dd48932
unknown <1486867975@qq.com>
2024-12-27 23:43:55 +0800

    ./

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -11,10 +11,10 @@
 <style scoped>
     .myHeader {
         color: black;
-        font: bold 30px Arial, sans-serif;
-        padding: 10px 10px 0px 10px;
+        font: bold 3rem Arial, sans-serif;
+        padding: 1rem 1rem 0rem 1rem;
         margin: 0%;
-        height: 50px;
+        height: 5rem;
         display: flex;
         justify-content: start;
         align-items: center;
